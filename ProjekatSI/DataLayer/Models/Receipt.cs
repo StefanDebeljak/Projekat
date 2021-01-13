690126f5dd1eba46fc689bfa781a00bfb145d31d
Aleksa <makljenovic99@gmail.com>
2021-01-13 15:59:35 +0100

    Kreirane klase modela za tabele iz baze.

---
+++ ProjekatSI/DataLayer/Models/Receipt.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+public class Receipt
+{
+	public int ReceiptId { get; set; }
+	public DateTime Date { get; set; }
+	public decimal TotalPrice { get; set; }
+}
---
+++ ProjekatSI/DataLayer/Models/Worker.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public class Worker
+{
+	public int WorkerId { get; set; }
+	public string FirstName { get; set; }
+	public string LastName { get; set; }
+	public string Password { get; set; }
+}
